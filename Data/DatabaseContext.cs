b53a3afefe657a881281907899bdfcd6b8e1d8c5
HectorFranc0u <halejandrofb@gmail.com>
2021-12-11 23:29:18 -0600

    ch-ch-ch changees~

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using webAPI.Models;
+
+namespace webAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() { }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options) { }
+
+        public DbSet<Furniture> furnitures {get;set;}   
+    }
+}
---
+++ Migrations/20211212032801_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace webAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "furnitures",
+                columns: table => new
+                {
+                    FurId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FurName = table.Column<string>(type: "text", nullable: true),
+                    FurType = table.Column<string>(type: "text", nullable: true),
+                    FurMaterial = table.Column<string>(type: "text", nullable: true),
+                    FurPrice = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_furnitures", x => x.FurId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "furnitures");
+        }
+    }
+}
